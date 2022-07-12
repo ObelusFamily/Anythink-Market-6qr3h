0fe5422a6694a94df24d5109daabab9197e8ba7b
dormeiri <dormeiri@gmail.com>
2022-07-12 23:09:20 +0300

    Seed fix

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.connect(process.env.MONGODB_URI);
@@ -5,7 +7,6 @@ mongoose.connect(process.env.MONGODB_URI);
 require("../models/User");
 require("../models/Item");
 require("../models/Comment");
-require("../config/passport");
 
 const User = mongoose.model("User");
 const Item = mongoose.model("Item");
@@ -13,26 +14,47 @@ const Comment = mongoose.model("Comment");
 
 const SEED_AMOUNT = 100;
 
-for(let i = 0; i < SEED_AMOUNT; i++) {
-    const seller = new User({
-        username: `user${i}`,
-        email: `user${i}@anythink.com`
+function seedUser(seedNumber) {
+    const user = new User({
+        username: `user${seedNumber}`,
+        email: `user${seedNumber}@anythink.com`
     });
-    seller.setPassword(`Password${i}`);
+    user.setPassword(`Password${seedNumber}`);
+    return user.save();
+}
 
-    const item = new Item({
-        title: `Item ${i}`,
-        description: `Some item ${i}`,
-        image: null,
-        tagList: [],
+function seedItem(seedNumber, seller) {
+    return Item({
+        title: `Item ${seedNumber}`,
         seller
-    });
+    }).save();
+}
 
-    const comment = new Comment({
-        body: `Comment ${i}`,
+function seedComment(seedNumber, seller, item) {
+    return new Comment({
+        body: `Comment ${seedNumber}`,
         item,
         seller
-    });
-
-    seller.save().then(() => item.save().then(() => comment.save()));
+    }).save();
 }
+
+async function seedOne(seedNumber) {
+    const seller = await seedUser(seedNumber);
+    const item = await seedItem(seedNumber, seller);
+    await seedComment(seedNumber, seller, item);
+}
+
+
+async function seed(amount) {
+    await Comment.deleteMany();
+    await Item.deleteMany();
+    await User.deleteMany();
+    for(let i = 0; i < amount; i++) {
+        console.log(`Seeding with ${i}...`);
+        await seedOne(i);
+    }
+}
+
+return seed(SEED_AMOUNT).then(() => {
+    process.exit(0);
+});
